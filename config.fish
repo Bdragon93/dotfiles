3dd7b6133f75fdf3c8bc37f4c1a853bcf3705804
Bdragon <bdragon@Dats-MacBook-Pro.local>
2020-01-19 23:03:14 +0700

    Backup at Sun Jan 19 23:03:14 +07 2020

--- config.fish
+++ config.fish
@@ -1,7 +1,7 @@
 alias python='python3'
 
 alias vi 'nvim'
-alias vimrc 'vi ~/.vimrc'
+alias vimrc 'vi ~/.config/nvim/init.vim'
 alias zshrc 'vi ~/.zshrc'
 alias alarc 'vi ~/.config/alacritty/alacritty.yml'
 alias trc 'vi ~/.tmux.conf'
@@ -11,19 +11,16 @@ alias q 'exit'
 alias cl 'clear'
 
 # goto
-alias gofr "cd ~/Projects/frontend-core"
-alias goeh "cd ~/Projects/employment-hero"
-alias gove "cd ~/Projects/react-velonic"
-alias gorb "cd ~/Projects/report-board"
-alias goau "cd ~/Projects/auth"
-alias gosq "cd ~/Projects/squad-health-check/frontend"
+alias gofr "cd ~/Projects/lxp-web"
+alias gom "cd ~/Projects/lxp-web-mobile"
 
 # yarn
 alias y "rm -rf node_modules && yarn"
 
 # git
 alias gpm "git checkout master && git pull origin master"
-alias gpld "git checkout development && git pull origin development"
+alias gpld "git checkout dev && git pull origin dev"
+alias gpod "git push origin dev"
 alias gco "git checkout"
 alias gaa "git add -A"
 alias ga "git add"
@@ -33,21 +30,24 @@ alias gplo "git pull origin"
 alias gmo "git merge origin"
 alias gfo "git fetch origin"
 alias gmo "git merge origin"
+alias gm "git merge"
 alias glog "git log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
 alias gacm "gaa && gcmm"
+
+# echo fish_vi_key_bindings
 
 # echo fish_vi_key_bindings
 # alias unvm="rnvm"
 
 # Use nvm
-# function rnvm 
-  # export NVM_DIR=~/.nvm
-  # source (brew --prefix nvm)/nvm.sh
-# end
+function rnvm 
+  export NVM_DIR=~/.nvm
+  source (brew --prefix nvm)/nvm.sh
+end
 
 # circleci
 function ci
-  open https://circleci.com/gh/Thinkei/$argv[1]
+  open https://circleci.com/gh/LeapXpert/$argv[1]
 end
 
 function cc
