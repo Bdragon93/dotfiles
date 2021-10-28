4fbb713a5dcb1bd3c44d3414fa7288b34c2f1df8
Bdragon <longtr268@gmail.com>
2021-10-28 17:19:42 +0700

    update restore

--- restore.sh
+++ restore.sh
@@ -1,6 +1,17 @@
 #!/bin/sh
 # Set up runtime config...
 git clone git@github.com:Bdragon93/dotfiles.git ~/Projects/dotfiles
+
+# Install Brew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# Install vim plug for neovim
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+curl -L https://get.oh-my.fish | fish
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.1/install.sh | bash
+
 mkdir -p ~/Projects
 cd ~/Projects/dotfiles
 
@@ -15,11 +26,7 @@ while read f; do
 done < Dotfiles
 echo "Copy files done!"
 
-
-# Install vim plug for neovim
-curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
+brew install node
 brew install tmux
 brew install --cask alacritty
 brew install neovim
@@ -28,9 +35,6 @@ brew install neovim
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 brew install fish
-
-curl -L https://get.oh-my.fish | fish
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.1/install.sh | bash
 
 omf install agnoster
 
